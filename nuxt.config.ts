c8f035a190943a6b2bd9883b1ebcdd20cac6e7ef
Yuan <sckwilson1008@gmail.com>
2026-01-20 23:32:41 +0800

    feat: 新增 @nuxtjs/sitemap 模組以支援網站地圖生成

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   ssr: false,
   app: {
-    baseURL: '/'
+    baseURL: "/",
   },
   devtools: { enabled: true },
 
   nitro: {
-    preset: 'github-pages'
+    preset: "github-pages",
   },
 
   modules: [
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-    '@vueuse/nuxt',
-    '@nuxt/icon'
-  ]
-})
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+    "@nuxtjs/sitemap",
+  ],
+
+  site: {
+    url: "https://napaincu.github.io/",
+    name: "前瞻AI人培-智慧代理及實體AI課程推動計畫",
+  },
+});
